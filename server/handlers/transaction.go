9c0f70d38691245fb0639aa397eb5e65787783da
Rakharan <randhikatamar@gmail.com>
2023-03-18 14:51:23 +0700

    trying to implement sendMail again

--- server/handlers/transaction.go
+++ server/handlers/transaction.go
@@ -172,9 +172,9 @@ func (h *handlerTransaction) Notification(c echo.Context) error {
 func SendMail(status string, transaction models.Transaction) {
 	if status != transaction.Status && (status == "success") {
 		// GET VARIABLES FROM ENV
-		var CONFIG_SMTP_HOST = os.Getenv("HOST_SYSTEM")
-		var CONFIG_SMTP_PORT, _ = strconv.Atoi(os.Getenv("PORT_SYSTEM"))
-		var CONFIG_SENDER_NAME = os.Getenv("SENDER_SYSTEM")
+		var CONFIG_SMTP_HOST = "smtp.gmail.com"
+		var CONFIG_SMTP_PORT = 587
+		var CONFIG_SENDER_NAME = "Waysbeans <shinonlime@gmail.com>"
 		var CONFIG_AUTH_EMAIL = os.Getenv("EMAIL_SYSTEM")
 		var CONFIG_AUTH_PASSWORD = os.Getenv("PASSWORD_SYSTEM")
 
